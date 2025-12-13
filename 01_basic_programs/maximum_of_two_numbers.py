077ae9b8775dd7b70ed649de65339755a105abc2
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-12-13 11:37:13 +0600

    solved max number Maximum of two numbers

---
+++ 01_basic_programs/maximum_of_two_numbers.py
@@ -0,0 +1,31 @@
+# Using max()
+"""a = 5
+b = 10
+max_number = max(a,b)
+print(max_number)"""
+
+# Using ternary operator
+"""a = 6
+b = 2
+res = a if a > b else b
+print(res)"""
+
+# Using if-Else statement
+"""a = 7
+b = 5
+
+if a > b:
+    print(a)
+else:
+    print(b)"""
+    
+# Using sort()
+"""a = 7 
+b = 3
+num = [a,b]
+# print(num)
+# print(num.sort())
+num.sort()
+res = num[-1]
+print(res)"""
+
