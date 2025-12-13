79fa7bba6356d251062b949692e6ae74335b5971
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-12-13 11:07:35 +0600

    DayOne --> twosum number

---
+++ day_one.py
@@ -0,0 +1,39 @@
+# Add Two Numbers - variable
+"""
+a = 20
+b = 30
+
+sum = a + b
+print(a+b)
+
+"""
+
+# Add Two Numbers - input
+"""
+a = input("First Number: ")
+b = input("Second Number: ")
+
+sum = int(a) + int(b)
+print(sum)
+"""
+
+# using function
+"""
+def twoSum(num1,num2):
+    return num1 + num2
+
+sum = twoSum(20,10)
+print(sum)
+"""
+# using lambda function
+"""sum =lambda a, b: a+b
+print(sum(2,3))"""
+
+# using operator
+"""
+    import operator
+print(operator.add(3,7))
+"""
+
+# using sum() - building funtion
+print(sum([4,5]))
