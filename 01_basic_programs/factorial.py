ae091e41dc27fe9a70a8488c4e7574dac9935282
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-12-15 14:45:43 +0600

    prob: solved factorial value using math module

---
+++ 01_basic_programs/factorial.py
@@ -0,0 +1,15 @@
+# Using math.factorial()
+"""import math
+ans = math.factorial(3)
+print(ans)"""
+
+# Using an Iterative For Loop
+"""num = 5 # 5 * 4 * 3 * 2 * 1 = 
+value = 1
+for ans in range(1, num + 1):
+    value = value * ans
+    print(value)
+"""
+
+# 
+
