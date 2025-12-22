4299f9856524561d2288ca7e0e2ffb9621df1e43
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-12-22 11:33:16 +0600

    solved: sum of array

--- 02_array_and_list/sum_list.py
+++ 02_array_and_list/sum_list.py
@@ -1,1 +1,15 @@
-#  121212dfa
+# Find Sum of Array
+
+"""array =  [2,777,4,3,6,3,224]
+ans = sum(array)
+print(ans)"""
+
+# second aproach
+"""array =  [1002,5]
+sum = 0
+# print(type(sum))
+for i in array:
+    # print(i,type(sum))
+    sum += i 
+    
+print(sum)"""
